36c75e607c99dbd99d08cc076cd2d216b6d25db8
YuriSouzaDev <yurisouza.developer@gmail.com>
2023-11-03 08:34:42 -0300

    feat: modal feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ const bookContainer = document.querySelector(".book-container");
 // Evento de envio do formulário
 function handleSubmit(event) {
   event.preventDefault();
+
   // Valide os campos e mostre os botões de erro, se necessário
   validateForm();
 }
@@ -30,64 +31,76 @@ Book.prototype.readStatus = function () {
 };
 
 // funtion para validacao de inputs
-function validateForm() {
+function validateForm(validate) {
   const title = form.inTitle.value;
   const author = form.inAuthor.value;
   const pages = form.inPages.value;
   const year = form.inYear.value;
-  const read = form.isRead.checked;
-  const isReadInput = document.getElementById("isRead");
-  const notReadInput = document.getElementById("notRead");
+  const isReadInput = document.querySelector("#isRead");
+  const notReadInput = document.querySelector("#notRead");
 
-  const titleError = document.getElementById("titleError");
-  const authorError = document.getElementById("authorError");
-  const pagesError = document.getElementById("pagesError");
-  const yearError = document.getElementById("yearError");
-  const readError = document.getElementById("readError");
+  const titleError = document.querySelector("#titleError");
+  const authorError = document.querySelector("#authorError");
+  const pagesError = document.querySelector("#pagesError");
+  const yearError = document.querySelector("#yearError");
+  const readError = document.querySelector("#readError");
 
-  let isValid = true;
+  validate = true;
 
   if (title.trim() === "") {
     titleError.style.color = "red";
-    isValid = false;
+    validate = false;
+  } else if (title.length > 41 || title.length === 0) {
+    titleError.innerHTML = "Please, insert between 1 - 40 characters";
+    titleError.style.color = "red";
+    validate = false;
   } else {
-    titleError.style.color = "white";
+    titleError.style.color = "#f7efe5";
   }
 
   if (author.trim() === "") {
     authorError.style.color = "red";
-    isValid = false;
+    validate = false;
+  } else if (author.length > 20 || author.length === 0) {
+    authorError.innerHTML = "Please, insert between 1 - 20 characters";
+    authorError.style.color = "red";
+    validate = false;
   } else {
-    authorError.style.color = "white";
+    authorError.style.color = "#f7efe5";
   }
 
   if (pages.trim() === "") {
     pagesError.style.color = "red";
-    isValid = false;
+    validate = false;
+  } else if (pages.length > 5 || pages.length === 0) {
+    pagesError.innerHTML = "Please, insert between 1 - 5 digits";
+    pagesError.style.color = "red";
+    validate = false;
   } else {
-    pagesError.style.color = "white";
+    pagesError.style.color = "#f7efe5";
   }
 
   if (year.trim() === "") {
     yearError.style.color = "red";
-    isValid = false;
-  } else if (year.length < 4 || year.length > 5) {
+    validate = false;
+  } else if (year.length < 4 || year.length > 4) {
     yearError.innerHTML = "Please, insert 4 digits";
     yearError.style.color = "red";
-    isValid = false;
+    validate = false;
   } else {
-    yearError.style.color = "white";
+    yearError.style.color = "#f7efe5";
   }
 
   if (!isReadInput.checked && !notReadInput.checked) {
     readError.style.color = "red";
-    isValid = false;
+    validate = false;
   } else {
-    readError.style.color = "white";
+    readError.style.color = "#f7efe5";
   }
 
-  if (isValid) {
+  if (validate) {
     addBookToLibrary();
+    closeModal();
   }
 }
 
@@ -113,18 +126,42 @@ function createBook(book) {
   const bookCard = `
   <article class="book-card">
   <div>
-  <h2 class="title">"${book.title}"</h2>
+  <h2 class="title">${book.title}</h2>
   <p class="year">${book.year}</p>
   </div>
   <div>
   <p class="author">By: ${book.author}</p>
   <p class="pages">Number of pages: ${book.pages}</p>
   </div>
-  <!-- <p>Already read</p> -->
+  <button class="${!book.read ? "not-Read" : ""}"}>${book.readStatus()}</button>
   </article>`;
 
   bookContainer.innerHTML += bookCard;
-  // bookContainer.appendChild(bookCard);
 }
 
 form.addEventListener("submit", handleSubmit);
+
+const buttonNewBook = document.querySelector(".newBook");
+const modal = document.querySelector(".modal");
+const buttonCloseModal = document.querySelector(".close-modal");
+
+function resetErrorColors() {
+  const errorMessage = document.querySelectorAll(".error-message");
+
+  errorMessage.forEach((error) => {
+    error.style.color = "#f7efe5";
+  });
+}
+
+function handleModal() {
+  modal.style.display = "flex";
+  resetErrorColors();
+}
+
+buttonNewBook.addEventListener("click", handleModal);
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+buttonCloseModal.addEventListener("click", closeModal);
